5fd04a337dd6fec1afc9bf53b437cce01e4fef9e
jburhenn <jburhenn@mydevices.com>
2017-06-15 10:58:27 -0600

    Clean up thread pool code.

--- myDevices/os/threadpool.py
+++ myDevices/os/threadpool.py
@@ -1,12 +1,20 @@
-from  concurrent.futures import ThreadPoolExecutor
+"""
+This module provides a singleton thread pool class
+"""
+from concurrent.futures import ThreadPoolExecutor
 from myDevices.utils.singleton import Singleton
-import inspect
 
 executor = ThreadPoolExecutor(max_workers=4)
 class ThreadPool(Singleton):
-	def Submit(something):
-		future = executor.submit(something)
-	def SubmitParam(*arg):
-		executor.submit(*arg)
-	def Shutdown():
-		executor.shutdown()
+    """Singleton thread pool class"""
+
+    @staticmethod
+    def Submit(func):
+        """Submit a function for the thread pool to run"""
+        executor.submit(func)
+
+    @staticmethod
+    def Shutdown():
+        """Shutdown the thread pool"""
+        executor.shutdown()
+        
