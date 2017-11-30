40cfc6098eb60804d68d1906567997e70d8072ef
jburhenn <jburhenn@mydevices.com>
2017-11-29 17:16:57 -0700

    Remove unneeded files.

--- package.sh
+++
@@ -1,8 +0,0 @@
-DEPLOY_DIR=Cayenne-Agent
-mkdir $DEPLOY_DIR
-cp -rf myDevices $DEPLOY_DIR
-cp -rf scripts $DEPLOY_DIR
-cp -rf setup.py $DEPLOY_DIR
-BUILD_NAME="myDevices-test.tar.gz"
-tar -czf $BUILD_NAME $DEPLOY_DIR
-rm -rf $DEPLOY_DIR
