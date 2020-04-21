259e1ce5f75d6a2269233bff0702bee8bd6f8ce5
jburhenn <jburhenn@mydevices.com>
2020-04-21 13:02:19 -0600

    Update version

--- myDevices/__init__.py
+++ myDevices/__init__.py
@@ -1,5 +1,5 @@
 """
 This package contains the Cayenne agent, which is a full featured client for the Cayenne IoT project builder: https://cayenne.mydevices.com. It sends system information as well as sensor and actuator data and responds to actuator messages initiated from the Cayenne dashboard and mobile apps.
 """
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 
